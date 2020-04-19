fc43162d287d7a44b89fa58918ec2c7c0bbbede5
moeinZ <moein459@gmail.com>
2020-04-19 09:43:29 +0430

    initialize sidebar library

---
+++ projects/sidebar/src/lib/sidebar.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {SidebarComponent} from './sidebar.component';
+
+
+@NgModule({
+	declarations: [SidebarComponent],
+	imports: [],
+	exports: [SidebarComponent]
+})
+export class SidebarModule {
+}
