affaa4e68dfa19e02d0bbfef0e28f85e0ed82fc0
moeinZ <moein459@gmail.com>
2020-08-01 13:07:04 +0430

    initial panel library

---
+++ projects/panel/src/lib/panel.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { PanelComponent } from './panel.component';
+
+
+
+@NgModule({
+  declarations: [PanelComponent],
+  imports: [
+  ],
+  exports: [PanelComponent]
+})
+export class PanelModule { }
