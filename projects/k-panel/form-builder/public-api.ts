dc5ab3c6ca3f0249752c6ce244b718daf2a5f881
mo3in <moein.hente@gmail.com>
2020-08-07 13:31:15 +0430

    wip: form-builder.module

--- projects/k-panel/form-builder/public-api.ts
+++ projects/k-panel/form-builder/public-api.ts
@@ -1,5 +1,7 @@
 export * from './form-builder.module';
 
+export * from './components/form-builder/form-builder.component'
+
 export * from './classes/i-component-descriptor';
 export * from './classes/icomponent';
 export * from './classes/ilayout-component';
