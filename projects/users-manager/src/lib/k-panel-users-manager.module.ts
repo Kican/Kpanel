cdcda09fb04d8aa8e5f0a6d2ab006f2e84a9b1f8
mo3in <moein.hente@gmail.com>
2020-09-04 03:24:49 +0430

    fix ENTITY_MANAGER_CONFIG

--- projects/users-manager/src/lib/k-panel-users-manager.module.ts
+++ projects/users-manager/src/lib/k-panel-users-manager.module.ts
@@ -3,13 +3,13 @@ import {KPanelCoreModule} from '@ngx-k-panel/core';
 import {UsersListPageComponent} from './components/users-list-page/users-list-page.component';
 import {RouterModule, Routes} from '@angular/router';
 import {KPanelEntityManagerModule} from '@ngx-k-panel/entity-manager';
-import {DataTableModule} from '@ngx-k-panel/data-table';
 import {KPanelFormBuilderModule} from '@ngx-k-panel/form-builder';
 import {DialogUpdatePasswordComponent} from './components/_dialogs/dialog-update-password/dialog-update-password.component';
 import {DialogSetUserRolesComponent} from './components/_dialogs/dialog-set-user-roles/dialog-set-user-roles.component';
 import {EditUserPageComponent} from './components/edit-user-page/edit-user-page.component';
 import {SidebarDynamicMenuService} from '@ngx-k/components/sidebar';
 import {NgbModalModule} from '@ng-bootstrap/ng-bootstrap';
+import {DataTableModule} from '@ngx-k/components/data-table';
 
 export const usersManagerRoutes: Routes = [
 	{
